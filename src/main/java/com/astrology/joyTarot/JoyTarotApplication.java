2c8c54c794dd6dbbda9865fb741e1d8cae63945c
Joanne <joan71739@gmail.com>
2024-10-24 13:38:52 +0800

    測試commit

--- src/main/java/com/astrology/joyTarot/JoyTarotApplication.java
+++ src/main/java/com/astrology/joyTarot/JoyTarotApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JoyTarotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JoyTarotApplication.class, args);
+		SpringApplication.run(JoyTarotApplication.class, args);  
 	}
 
 }
